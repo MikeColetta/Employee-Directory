202b3817f3f4a4f19e79ea337e79fe1938fd9aeb
Mike Coletta <coletta.mike@gmail.com>
2021-05-03 14:32:03 -0700

    Made minimum employee render amount of 10

--- employeedirectory/src/utils/API.js
+++ employeedirectory/src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const employeeAmount = Math.floor(Math.random() * 20 + 1)
+const employeeAmount = Math.floor(Math.random() * 20 + 9)
 const URL = `https://randomuser.me/api/?results=${employeeAmount}`
 
 export default {
